f8b0a8a9ecea9e94f9f80bb3080b4b76ba0062e0
Lupe <lupec_se@yahoo.com>
2018-11-10 10:00:40 -0700

    initial changes to HTML

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyAYXQ2PccNf1Cf4mQDYOpEgx6TZ7UvYXyg",
+    authDomain: "firstproject-40c29.firebaseapp.com",
+    databaseURL: "https://firstproject-40c29.firebaseio.com",
+    projectId: "firstproject-40c29",
+    storageBucket: "firstproject-40c29.appspot.com",
+    messagingSenderId: "812777626258"
+};
+firebase.initializeApp(config);
+
